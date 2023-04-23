504d35e8b69a0e6df59eaa20e03553fac3d6d8df
diannita <ingeniera.dianam@gmail.com>
2023-04-23 21:56:31 +1200

    Lesson 5 - Side-effects, Custom Hooks and Fragments)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import * as React from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
+// Custom hook for semi-persistent state
+function useSemiPersistentState(key, initialState) {
+  const [state, setState] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, state);
+  }, [key, state]);
+
+  return [state, setState];
+}
+
 function App() {
-  const [todoList, setTodoList] = React.useState([]);
+  // Update to use custom hook useSemiPersistentState
+  const [todoList, setTodoList] = useSemiPersistentState(
+    "savedTodoList",
+    JSON.stringify([])
+  );
 
   const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo]);
+    setTodoList(JSON.stringify([...JSON.parse(todoList), newTodo]));
   };
 
   return (
@@ -14,7 +31,7 @@ function App() {
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
       <hr />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={JSON.parse(todoList)} />
       <hr />
     </div>
   );
