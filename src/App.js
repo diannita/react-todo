4ff83256d9a2675180942c68638dddc0520b0c15
Diana Rodriguez Cosme <76630368+diannita@users.noreply.github.com>
2023-08-05 09:52:53 +1200

    lesson 1.8 done

--- src/App.js
+++ src/App.js
@@ -2,26 +2,50 @@ import * as React from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
+
 function App() {
   const key = "savedTodoList";
   const [todoList, setTodoList] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
-  React.useEffect(() => {
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          data: {
-            todoList: JSON.parse(localStorage.getItem(key)) || [],
-          },
-        });
-      }, 2000);
-    }).then((result) => {
-      const { todoList } = result.data;
-      setTodoList(todoList);
-      setIsLoading(false);
-    });
+  async function fetchData() {
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+      },
+    };
+
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
+
+    try {
+      const response = await fetch(url, options);
+      console.log(response);
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      } else{
+      const data = await response.json();
+      console.log("Data:", data); // Print the API response data
+      return data;
+      }
+
+    } catch (error) {
+      console.log(error); // Log the error message for debugging
+    }
+  }
+
+  React.useEffect(()=>{
+    fetchData().then((data) => {
+    const todos = data.records.map((item)=>{
+      return {id:item.id, title:item.fields.title};
+    })
+    setTodoList(todos);
+    console.log('Data ' + todos);
+    setIsLoading(false);
+   });
   }, []);
+
 
   React.useEffect(() => {
     if (!isLoading) {
